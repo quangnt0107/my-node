81cfb0c0d53091cc9557bc36552f34bcb1a55316
quangnt0107 <quangnt0107@gmail.com>
2023-03-02 19:20:08 +0700

    add mock data josn

--- db.js
+++ db.js
@@ -13,6 +13,13 @@ const listDatabases = async (client) => {
 async function createListing(client, newListing){
   const result = await client.db(dbName).collection("listingsAndReviews").insertOne(newListing);
   console.log(`New listing created with the following id: ${result.insertedId}`);
+};
+
+async function createMultipleListings(client, newListings){
+  const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListings);
+
+  console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
+  console.log(result.insertedIds);       
 }
 
 
@@ -29,10 +36,10 @@ const connectDB = async () => {
     // create
     await createListing(client,
       {
-          name: "Lovely Loft",
-          summary: "A charming loft in Paris",
-          bedrooms: 1,
-          bathrooms: 1
+        name: "Lovely Loft",
+        summary: "A charming loft in Paris",
+        bedrooms: 1,
+        bathrooms: 1
       }
   );
 
